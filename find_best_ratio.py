7b34182d9cf1f03dc38de178007b69ecacc5da81
Vladimir Yashunsky <vladimir.yashunsky@gmail.com>
2018-06-17 20:24:38 +0300

    prepare model for optimization

---
+++ find_best_ratio.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+import numpy as np
+
+from work_area_model import HindLeg, vec, angles_generator
+
+
+def model_factory(base, angles, resolution, mask=vec(True, True)):
+    def get_model(knee_rod, knee_connection_rod, knee_offset, hip, shin):
+        max_length = hip + shin - knee_offset
+        hing_leg = HindLeg(base,
+                           knee_rod, knee_connection_rod, knee_offset,
+                           hip, shin)
+        result = hing_leg.get_max_cross_sections(angles, resolution)[mask]
+        result /= max_length
+        return result[0] if len(result) == 1 else result
+    return get_model
+
+
+if __name__ == '__main__':
+
+    ANGULAR_RESOLUTION = 5.0
+    SPATIAL_RESOLUTION = 10.0
+
+    BASE = 20.0
+    INITIAL_KNEE_ROD = 20.0
+    INITIAL_KNEE_CONNECTION_ROD = 70.0
+    INITIAL_KNEE_OFFSET = 20.0
+    INITIAL_HIP = 70.0
+    INITIAL_SHIN = 150.0
+
+    MASK = vec(False, True)
+
+    ANGLES = list(angles_generator(hip_step=ANGULAR_RESOLUTION,
+                                   knee_step=ANGULAR_RESOLUTION))
+
+    model = model_factory(BASE, ANGLES, SPATIAL_RESOLUTION, mask=MASK)
+
+    vertical_usage = model(INITIAL_KNEE_ROD,
+                           INITIAL_KNEE_CONNECTION_ROD,
+                           INITIAL_KNEE_OFFSET,
+                           INITIAL_HIP,
+                           INITIAL_SHIN)
+
+    print vertical_usage
